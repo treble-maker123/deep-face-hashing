10505de47160c0a0c198c1f05deee3e3d77ec694
Ziqiang Guan <wolfgang.guan56@gmail.com>
2018-12-10 22:36:35 -0500

    added code to visualize predicted images

--- viz.py
+++ viz.py
@@ -6,35 +6,28 @@ from facenet import *
 
 CODES_PATH = "./codes"
 CODES_FILE = "/12-08_16-14-22_BA977C.codes"
-IMAGE_PATHS = "./image_paths.pickle"
+DATASET_PATHS = "./dataset.pickle"
 
 # how many test subjects to pick up and examine
-NUM_TEST_TO_SHOW = 5
+NUM_TEST_TO_SHOW = 6
+TOP_N_RESULTS = 10
 
 if __name__ == "__main__":
     with open(CODES_PATH + CODES_FILE, "rb") as file:
         codes = pickle.load(file)
 
-    gallery_codes, gallery_label, test_codes, test_label = codes
+    gallery_codes, gallery_labels, test_codes, test_labels = codes
     gallery_codes = np.array(gallery_codes)
-    gallery_label = np.array(gallery_label)[:, 0]
     test_codes = np.array(test_codes)
-    test_label = np.array(test_label)[:, 0]
 
-    with open(IMAGE_PATHS, "rb") as file:
-        gallery_paths, test_paths = pickle.load(file)
+    gallery_labels = gallery_labels.numpy()
+    test_labels = test_labels.numpy()
+    truth_table = gallery_labels == test_labels.T
+
+    with open(DATASET_PATHS, "rb") as file:
+        gallery, test = pickle.load(file)
 
     num_gallery, num_test = len(gallery_codes), len(test_codes)
-
-    # reload the dataset without transformation
-    gallery = FaceScrubDataset(type="label",
-                               mode="train",
-                               normalize=False,
-                               hash_dim=HASH_DIM)
-    test = FaceScrubDataset(type="label",
-                            mode="test",
-                            normalize=False,
-                            hash_dim=HASH_DIM)
 
     # only looking at a subset of test subjects
     test_idx = np.random.randint(0, num_test, num_test)[:NUM_TEST_TO_SHOW]
@@ -44,5 +37,29 @@ if __name__ == "__main__":
     # get the sorted idx
     sorted_idx = hamming_dist.argsort(axis=0)
 
-    # get the images of the selected test subjects
-    test_imgs = []
+    fig, ax_arr = plt.subplots(NUM_TEST_TO_SHOW, TOP_N_RESULTS+1,
+                               figsize=(25,25))
+
+    for i, tidx in enumerate(test_idx):
+        assert test_labels[tidx, 0] == test[tidx][1], "Mismatched test labels!"
+
+        # display the image
+        test_img = test[tidx][0]
+        ax_arr[i, 0].imshow(np.asarray(test_img))
+        ax_arr[i, 0].axis("off")
+        ax_arr[i, 0].set_title("Query")
+
+        # display the top N images
+        gallery_idx = sorted_idx[:TOP_N_RESULTS, i]
+
+        for j, gidx in enumerate(gallery_idx):
+            gallery_img = gallery[gidx][0]
+            ax_arr[i, j+1].imshow(np.asarray(gallery_img))
+            ax_arr[i, j+1].axis("off")
+
+            if truth_table[gidx, tidx]:
+                ax_arr[i, j+1].set_title("MATCH", color="g")
+            else:
+                ax_arr[i, j+1].set_title("MISMATCH", color="r")
+
+    plt.show()

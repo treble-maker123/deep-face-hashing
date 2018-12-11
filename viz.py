ccd096a0b100f80e930830ea88e1d67a73eb5839
Ziqiang Guan <wolfgang.guan56@gmail.com>
2018-12-10 21:07:04 -0500

    removed image paths

--- viz.py
+++ viz.py
@@ -6,6 +6,7 @@ from facenet import *
 
 CODES_PATH = "./codes"
 CODES_FILE = "/12-08_16-14-22_BA977C.codes"
+IMAGE_PATHS = "./image_paths.pickle"
 
 # how many test subjects to pick up and examine
 NUM_TEST_TO_SHOW = 5
@@ -20,17 +21,20 @@ if __name__ == "__main__":
     test_codes = np.array(test_codes)
     test_label = np.array(test_label)[:, 0]
 
+    with open(IMAGE_PATHS, "rb") as file:
+        gallery_paths, test_paths = pickle.load(file)
+
     num_gallery, num_test = len(gallery_codes), len(test_codes)
 
     # reload the dataset without transformation
     gallery = FaceScrubDataset(type="label",
-                             mode="train",
-                             normalize=False,
-                             hash_dim=HASH_DIM)
-    test = FaceScrubDataset(type="label",
-                               mode="test",
+                               mode="train",
                                normalize=False,
                                hash_dim=HASH_DIM)
+    test = FaceScrubDataset(type="label",
+                            mode="test",
+                            normalize=False,
+                            hash_dim=HASH_DIM)
 
     # only looking at a subset of test subjects
     test_idx = np.random.randint(0, num_test, num_test)[:NUM_TEST_TO_SHOW]
